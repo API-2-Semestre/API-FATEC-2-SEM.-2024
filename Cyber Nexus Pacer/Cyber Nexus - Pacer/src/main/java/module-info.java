695b778af0dc65ff96a2cb635eee5b1c42953582
Joao Pedro <127329098+jaupventur@users.noreply.github.com>
2024-09-30 17:51:27 -0300

    Delete Cyber Nexus Pacer directory

--- Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.cyber.cybernexuspacer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.cyber.cybernexuspacer to javafx.fxml;
-    exports com.cyber.cybernexuspacer;
-}
